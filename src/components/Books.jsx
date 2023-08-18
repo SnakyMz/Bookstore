1791d70ea3798434c1f98508b0d1277cd71cee8c
SnakyMz <Soban.syeed@outlook.com>
2023-08-19 03:48:48 +0530

    Add Book component

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,24 +1,25 @@
 import AddBook from './AddBook';
+import Book from './Book';
 
 function Books() {
   const books = [
     {
       id: 1,
-      name: 'The Hunger Games',
+      title: 'The Hunger Games',
       author: 'Suzanne Collins',
       progress: 64,
       chapter: 'Chapter 17',
     },
     {
       id: 2,
-      name: 'Dune',
+      title: 'Dune',
       author: 'Frank Herbert',
       progress: 8,
       chapter: 'Chapter 3: A Lesson Learned',
     },
     {
       id: 3,
-      name: 'Capital in the Twenty-First Century',
+      title: 'Capital in the Twenty-First Century',
       author: 'Suzanne Collins',
       progress: 0,
       chapter: 'Introduction',
@@ -29,31 +30,12 @@ function Books() {
       <ul id="books">
         {books.map((book) => (
           <li key={book.id}>
-            <div className="bookDesc">
-              <h3>{book.name}</h3>
-              <h4>{book.author}</h4>
-              <div className="bookBtns">
-                <button type="button" className="Btn">Comments</button>
-                <span>|</span>
-                <button type="button" className="Btn">Edit</button>
-                <span>|</span>
-                <button type="button" className="Btn">Delete</button>
-              </div>
-              <div className="bookProgress">
-                <div className="percentage">
-                  <h3>
-                    {book.progress}
-                    %
-                  </h3>
-                  <h4>Completed</h4>
-                </div>
-              </div>
-              <div className="bookChapter">
-                <h4>Current Chapter</h4>
-                <h3>{book.chapter}</h3>
-                <button type="button" className="Btn">Update Progress</button>
-              </div>
-            </div>
+            <Book
+              title={book.title}
+              author={book.author}
+              progress={book.progress}
+              chapter={book.chapter}
+            />
           </li>
         ))}
       </ul>
