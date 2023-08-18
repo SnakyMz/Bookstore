ab4443a4f65dadb4b7b109fb85736c227f1df1b1
SnakyMz <Soban.syeed@outlook.com>
2023-08-19 01:55:22 +0530

    Add and fix linters

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,60 +1,61 @@
-import AddBook from "./AddBook";
+import AddBook from './AddBook';
 
 function Books() {
   const books = [
     {
       id: 1,
-      name: "The Hunger Games",
-      author: "Suzanne Collins",
+      name: 'The Hunger Games',
+      author: 'Suzanne Collins',
       progress: 64,
-      chapter: "Chapter 17"
+      chapter: 'Chapter 17',
     },
     {
       id: 2,
-      name: "Dune",
-      author: "Frank Herbert",
+      name: 'Dune',
+      author: 'Frank Herbert',
       progress: 8,
-      chapter: "Chapter 3: A Lesson Learned"
+      chapter: 'Chapter 3: A Lesson Learned',
     },
     {
       id: 3,
-      name: "Capital in the Twenty-First Century",
-      author: "Suzanne Collins",
+      name: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
       progress: 0,
-      chapter: "Introduction"
+      chapter: 'Introduction',
     },
   ];
   return (
     <section id="library">
       <ul id="books">
-        {books.map((book, index) => {
-          return (
-            <li key={index} id={book.id}>
-              <div className="bookDesc">
-                <h3>{book.name}</h3>
-                <h4>{book.author}</h4>
-                <div className="bookBtns">
-                  <button type="button" className="Btn">Comments</button>
-                  <span>|</span>
-                  <button type="button" className="Btn">Edit</button>
-                  <span>|</span>
-                  <button type="button" className="Btn">Delete</button>
-                </div>
-                <div className="bookProgress">
-                  <div className="percentage">
-                    <h3>{book.progress}%</h3>
-                    <h4>Completed</h4>
-                  </div>
-                </div>
-                <div className="bookChapter">
-                  <h4>Current Chapter</h4>
-                  <h3>{book.chapter}</h3>
-                  <button type="button" className="Btn">Update Progress</button>
+        {books.map((book) => (
+          <li key={book.id}>
+            <div className="bookDesc">
+              <h3>{book.name}</h3>
+              <h4>{book.author}</h4>
+              <div className="bookBtns">
+                <button type="button" className="Btn">Comments</button>
+                <span>|</span>
+                <button type="button" className="Btn">Edit</button>
+                <span>|</span>
+                <button type="button" className="Btn">Delete</button>
+              </div>
+              <div className="bookProgress">
+                <div className="percentage">
+                  <h3>
+                    {book.progress}
+                    %
+                  </h3>
+                  <h4>Completed</h4>
                 </div>
               </div>
-            </li>
-          );
-        })}
+              <div className="bookChapter">
+                <h4>Current Chapter</h4>
+                <h3>{book.chapter}</h3>
+                <button type="button" className="Btn">Update Progress</button>
+              </div>
+            </div>
+          </li>
+        ))}
       </ul>
       <AddBook />
     </section>
