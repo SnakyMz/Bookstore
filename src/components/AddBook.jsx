cd60bafe5f3e1fb59c55ad93ed0f82994b13eef5
SnakyMz <Soban.syeed@outlook.com>
2023-08-22 20:50:53 +0530

    Add remove and add book buttons

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -6,18 +6,19 @@ function AddBook() {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
   const formSubmit = (e) => {
     e.preventDefault();
     const book = {
       id: Date.now(),
       title,
       author,
-      progress: 64,
-      chapter: 'Chapter 1',
+      category,
     };
     dispatch(addBook(book));
     setTitle('');
     setAuthor('');
+    setCategory('');
   };
   return (
     <section id="addBook">
@@ -28,17 +29,35 @@ function AddBook() {
           id="title"
           name="title"
           value={title}
-          placeholder=" Title"
+          placeholder="Title"
+          aria-label="title"
           onChange={(e) => setTitle(e.target.value)}
         />
         <input
           type="text"
           id="author"
           name="author"
-          placeholder=" Author"
+          placeholder="Author"
           value={author}
+          aria-label="author"
           onChange={(e) => setAuthor(e.target.value)}
         />
+        <select
+          name="category"
+          id="category"
+          aria-label="category"
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="fiction">Fiction</option>
+          <option value="nonfiction">Nonfiction</option>
+          <option value="romance">Romance</option>
+          <option value="horror">Horror</option>
+          <option value="biography">Biography</option>
+          <option value="fantasy">Fantasy</option>
+          <option value="comedy">Comedy</option>
+          <option value="thriller">Thriller</option>
+          <option value="drama">Drama</option>
+        </select>
         <button type="submit">Submit</button>
       </form>
     </section>
